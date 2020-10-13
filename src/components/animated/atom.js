84e513b28043c6302c9d4394aab630c651c316f3
mlacosta <marianoacosta.003@gmail.com>
2020-10-12 22:53:08 -0300

    fix react bar

--- src/components/animated/atom.js
+++ src/components/animated/atom.js
@@ -6,9 +6,9 @@ function Atom(){
     return(
         <svg width="200" height="200">
             <g>
-                <ellipse cx="100" cy="100" rx="22" ry="8" />
-                <ellipse cx="100" cy="100" rx="22" ry="8" transform="rotate(60, 100, 100)" />
-                <ellipse cx="100" cy="100" rx="22" ry="8" transform="rotate(120, 100, 100)" />
+                <ellipse cx="100" cy="100" rx="20" ry="7" />
+                <ellipse cx="100" cy="100" rx="20" ry="7" transform="rotate(60, 100, 100)" />
+                <ellipse cx="100" cy="100" rx="20" ry="7" transform="rotate(120, 100, 100)" />
                 <circle cx="100" cy="100" r="4" />
             </g>
         </svg>
@@ -18,7 +18,6 @@ function Atom(){
 function AtomBar(){
     return(
         <div className="AtomBar"><Atom/><a href="https://reactjs.org/" target='_blank'><span id='react-msg'>Powered by React.js</span></a></div>
-        
     )
 }
 
