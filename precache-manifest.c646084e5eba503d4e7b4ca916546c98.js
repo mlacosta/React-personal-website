a9f3e405b732f2f1c56047408964f838d449a683
mlacosta <marianoacosta.003@gmail.com>
2020-10-13 16:35:51 -0300

    Updates

---
+++ precache-manifest.c646084e5eba503d4e7b4ca916546c98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037ee765015cf1c28e2908c1f12dd222",
+    "url": "/React-personal-website/index.html"
+  },
+  {
+    "revision": "285011128d04836d0292",
+    "url": "/React-personal-website/static/css/main.7cab5601.chunk.css"
+  },
+  {
+    "revision": "01b0f5c7569e978cee00",
+    "url": "/React-personal-website/static/js/2.ed418c21.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/React-personal-website/static/js/2.ed418c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "285011128d04836d0292",
+    "url": "/React-personal-website/static/js/main.1377d75e.chunk.js"
+  },
+  {
+    "revision": "40cc684c6ee4bda672d1",
+    "url": "/React-personal-website/static/js/runtime-main.c5a3e072.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-personal-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
