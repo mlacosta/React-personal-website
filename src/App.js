6b6837e6e38bde7e32129764a976f1049bc0cdeb
mlacosta <marianoacosta.003@gmail.com>
2020-10-25 00:15:38 -0300

    update info

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
     super(props);
     let index = Math.floor(Math.random() * Math.floor(5));
     //index = 5; //debug purposes
-    this.state = {width: window.innerWidth, height: window.innerHeight, colorPalette:themes[index]};
+    this.state = {width: window.innerWidth, height: window.innerHeight, colors:themes[index]};
     this.handleResize = this.handleResize.bind(this);
   }
 
@@ -26,47 +26,29 @@ class App extends React.Component {
     this.setState({width: window.innerWidth, height: window.innerHeight });
     let index = Math.floor(Math.random() * Math.floor(5));
     //index = 5; //debug purposes
-    this.setState({colorPalette:themes[index]});
+    this.setState({colors:themes[index]});
   }
 
   render(){
 
     const body = document.getElementsByTagName("BODY")[0];
-    body.setAttribute("style", `background-color: ${this.state.colorPalette.background}`);
-
+    body.setAttribute("style", `background-color: ${this.state.colors.background}`);
     window.addEventListener('resize', this.handleResize);
-
-    const particleStyle = ((this.state.colorPalette.name === 'no end') || (this.state.colorPalette.name === 'indo silver club'))? {
-        particles:{
-          color: {
-            value: '#20a9b1'
-          },
-          "line_linked": {
-            color:'#20a9b1'
-          }
-        }
-      } : {
-          //add another option if you want
-      }
-
+    
+    let isTheme = ((this.state.colors.name === 'no end') || (this.state.colors.name === 'indo silver club'));
+    const particleStyle = isTheme && {particles:{color: {value: '#20a9b1'},"line_linked": {color:'#20a9b1'}}}; 
+    
     return (
       <div className="App" >
-        <Particles 
-          className="particles" 
+        <Particles className="particles" 
           width = {this.state.width} 
           height = {this.state.height} 
           style = {{position:'fixed'}}
-          params = {particleStyle}
-        />
+          params = {particleStyle}/>
         <PageContainer>
-          <MainBox 
-            colors = {this.state.colorPalette} 
-            contact = {links} 
-            width = {this.state.width} 
-            height = {this.state.height}>
-          </MainBox>
+          <MainBox {...this.state} contact = {links}/> 
           <AtomBar/>
-          <Display colors = {this.state.colorPalette}/>
+          <Display colors = {this.state.colors}/>
         </PageContainer>
       </div>
     );
