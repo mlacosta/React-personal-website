efc8b7c62987d182d3902a632b8cf14cbc770594
mlacosta <marianoacosta.003@gmail.com>
2020-11-03 01:02:50 -0300

    mouse tracking

--- src/components/debug-window/debug-window.js
+++ src/components/debug-window/debug-window.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './debug-window.css';
+import useMousePosition from '../misc/UseMousePosition/UseMousePosition';
 
 
 function DebugWindow ( { width, height, name} ){
-
+    const { x, y } = useMousePosition();
     return(
         <div className="DebugWindow">
             <span id='debug-title'>Debug mode</span><br/>
-            x-coor: {width} <br/>
-            y-coor: {height} <br/>
+            width: {width} <br/>
+            height: {height} <br/>
+            x-axis: {x} <br/>
+            y_axis: {y} <br/>
             theme: {name}
         </div>
     )
